9275a846479f82178eb2b4be0ad4bc391c529992
cgouin00 <50848365+cgouin00@users.noreply.github.com>
2020-06-27 14:39:16 -0400

    Referrer Update

--- assets/js/updated.js
+++ assets/js/updated.js
@@ -1,4 +1,4 @@
-var msg = '<b>Last Updated: </b>' + document.lastModified + '</p>';
+var msg = '<b>Last Updated: &nbsp; </b>' + document.lastModified + '</p>';
 
 var el = document.getElementById('updated');
 el.innerHTML = msg;
